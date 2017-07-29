ce81f28b711c68fd539fcf5f819b72004979f980
Rialgar <rialgar42@gmail.com>
2017-07-29 10:35:42 +0200

    Initial Commit - Basic Playground setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# made for LD39
+
+Not playable, yet.
+
+Will be playable here: https://rialgar.github.io/LD39/index.xhtml
+
+When cloning, just serve the directory with your favourite webserver and navigate to index.xhtml
---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+	var app = playground({
+		render: function() {
+
+			this.layer.clear("#000088");
+			this.layer.fillStyle("#ffffff");
+			this.layer.fillRect(32, 32, 64, 64);
+		}
+	});
+})
